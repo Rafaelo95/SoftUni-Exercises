64d2f24ac7b8bf61925ccb17810e5d3abaa2d06f
Rafaelo Antonov <72706604+Rafaelo95@users.noreply.github.com>
2021-03-22 20:45:56 +0200

    Delete stacks-and-queues directory

--- stacks-and-queues/reverse-numbers-with-stack.py
+++
@@ -1,7 +0,0 @@
-numbers_stack = list(input().split())
-reversed_numbers_stack = []
-
-for i in range(len(numbers_stack)):
-    reversed_numbers_stack.append(numbers_stack.pop())
-
-print(' '.join(reversed_numbers_stack))
