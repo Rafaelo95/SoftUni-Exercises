2e213938fed5c873eac183835988f5134ee111f6
Rafaelo Antonov <72706604+Rafaelo95@users.noreply.github.com>
2021-03-22 20:45:27 +0200

    Delete even_lines directory

--- even_lines/even-lines.py
+++
@@ -1,8 +0,0 @@
-with open("text.txt", "r") as file:
-    for idx, line in enumerate(file):
-        if idx % 2 == 0:
-            for element in "-,.!?":
-                line = line.replace(element, "@")
-
-            words = reversed(line.split())
-            print(' '.join(words))
