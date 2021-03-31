23b4379451613e320deafb52630dbb97f1233bae
Rafaelo Antonov <72706604+Rafaelo95@users.noreply.github.com>
2021-03-31 12:57:41 +0300

    Delete exam 22 aug 2020 directory

--- exam 22 aug 2020/project/people/child.py
+++
@@ -1,7 +0,0 @@
-class Child:
-    def __init__(self, food_cost: int, *args):
-        result = food_cost + sum(args)
-        self.cost = result
-
-    def get_monthly_expense(self):
-        return self.cost * 30
