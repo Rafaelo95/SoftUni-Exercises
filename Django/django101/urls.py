8778615edf14209d63d7a7ad77f6601ec02d425a
Rafaelo Antonov <72706604+Rafaelo95@users.noreply.github.com>
2021-06-24 00:48:12 +0300

    Delete Django directory

--- Django/django101/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('django102.urls')),
-    path('', include('django101_admin.urls'))
-]
--- Django/django102/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from django102.views import index as index_view, GamesListView, something
-
-urlpatterns = [
-    path('', index_view),
-    path('games/', GamesListView.as_view()),
-    path('smth/', something)
-]
