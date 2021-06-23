3c719d27db6df44984034d54e0e02badac017f51
Rafaelo Antonov <72706604+Rafaelo95@users.noreply.github.com>
2021-06-24 00:48:01 +0300

    Delete JavaScript/Basic Syntax directory

--- JavaScript/Basic Syntax/demo.js
+++
@@ -1,9 +0,0 @@
-let newArray = [10, 5, 4, 1, 25, 15];
-
-let [firstNum, ...other] = newArray;
-
-console.log(firstNum);
-
-for(let i = 0; i < other.length; i++) {
-    console.log(other[i]);
-}
--- JavaScript/Basic Syntax/fruit.js
+++
@@ -1,8 +0,0 @@
-function calculate(fruit, weightGrams, priceKilogram) {
-    let weightKilogram = weightGrams / 1000
-    let moneyNeeded = weightKilogram * priceKilogram
-
-    console.log(`I need $${moneyNeeded.toFixed(2)} to buy ${weightKilogram.toFixed(2)} kilograms ${fruit}.`)
-}
-
-calculate('orange', 2500, 1.80)
